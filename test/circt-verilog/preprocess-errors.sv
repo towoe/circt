e47b65548aac4ab90a732389fc80085a5438b338
Tobias Wölfel <tobias.woelfel@mailbox.org>
2025-04-07 12:41:28 +0000

    [test] Remove error string matching
    The expexted error message is OS dependant. Remove the text. It will
    still expect the test to return with an error.

--- test/circt-verilog/preprocess-errors.sv
+++ test/circt-verilog/preprocess-errors.sv
@@ -1,5 +1,5 @@
 // RUN: circt-verilog %s -E --verify-diagnostics
 // REQUIRES: slang
 
-// expected-error @below {{'unknown.sv': No such file or directory}}
+// expected-error @below {{}}
 `include "unknown.sv"
